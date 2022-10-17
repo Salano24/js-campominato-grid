b3d79b54edc755b57354b457f25b6d067d1de72d
Salano24 <salernomarco48@email.com>
2022-10-17 16:26:39 +0200

    correzzione e indentazione corretta

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,35 @@
 //L'utente clicca su un bottone che genererà una griglia di gioco quadrata. Ogni cella ha un numero progressivo, da 1 a 100.
 
-const genera = document.querySelector('.genera')
-genera.addEventListener('click', function(){
-/* seleziono il container */
-let container = document.querySelector(".container")
-/* dichiaro il numero di caselle */
-let numeroCaselle = 100
-/* seleziono i div 'casella' */
-let listaCaselle = document.getElementsByClassName('casella')
-console.log(listaCaselle);
-/* richiamo la funzione per generare la casella */
-caselleGenerator(numeroCaselle,container)
-/* scrivo la funzione per generare la casella */
-function caselleGenerator(numeroCaselle,container){
-    for(let i = 0; i < numeroCaselle; i++){
-        container.innerHTML += `<div class='casella' id="${i}"></div>`
-    }
+const genera = document.querySelector('.genera');
+    genera.addEventListener('click', function(){
+    /* seleziono il container */
+        let container = document.querySelector(".container");
     
-    for(let i = 0; i < listaCaselle.length; i++){
-        let casellaAttuale = listaCaselle[i]
-        casellaAttuale.addEventListener("click", function(){
-            casellaAttuale.classList.toggle('azzurro')
-            console.log(casellaAttuale.id)
-        })
-    }
-}
-})
+        /* dichiaro il numero di caselle */
+        let numeroCaselle = 100;
+    
+    
+        /* richiamo la funzione per generare la casella */
+        caselleGenerator(numeroCaselle,container);
+        console.log(listaCaselle);
+    
+        /* scrivo la funzione per generare la casella */
+        function caselleGenerator(numeroCaselle,container){
+            for(let i = 0; i < numeroCaselle; i++){
+                container.innerHTML += `<div class='casella' id="${i}"></div>`;
+            };
+            /* seleziono i div 'casella' */
+        let listaCaselle = document.querySelectorAll('.casella');
+            
+            for(let i = 0; i < listaCaselle.length; i++){
+                let casellaAttuale = listaCaselle[i];
+                casellaAttuale.addEventListener("click", function(){
+                    casellaAttuale.classList.toggle('azzurro');
+                    console.log(casellaAttuale.id);
+                });
+            };
+        };
+});
 
 //Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 
